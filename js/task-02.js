1d856da7384bffbb527c9950245ab56ab1f0a080
Maryna Berezhanska <marina@MacBook-Pro-Marina.local>
2022-04-15 00:15:24 +0300

    task #3

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const items = [];
+
+for(let i=0; i < ingredients.length;i++){
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredients[i];
+  items.push(itemEl);
+};
+
+const listEl = document.querySelector('ul');
+listEl.append(...items);
+
+
+
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,33 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// #1
+
+const listEl = document.querySelector('ul');
+
+for(let i=0; i < images.length;i++){
+  listEl.insertAdjacentHTML('beforeend', `<li class="gallery-item"><img class="gallery-image" src="${images[i].url}" alt="${images[i].alt}"></li>`);
+};
+
+
+// #2
+// const items = [];
+// const listEl = document.querySelector('ul');
+
+// for(let i=0; i < images.length;i++){
+  // const itemEl = document.createElement('li');
+  // itemEl.classList.add('gallery-item');
+  // const itemImageEl = document.createElement('img');
+  // itemImageEl.classList.add('gallery-image');
+  // itemImageEl.src = images[i].url;
+  // itemImageEl.alt = images[i].alt;
+  // itemEl.append(itemImageEl);
+
+  // items.push(itemEl);
+// };
+
+// listEl.append(...items);
+
+
+
