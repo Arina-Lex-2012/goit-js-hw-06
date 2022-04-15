a3a09f27153ebfa81f7f3c3bf10f48fc6eba1429
Maryna Berezhanska <marina@MacBook-Pro-Marina.local>
2022-04-15 12:00:17 +0300

    task #5

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector('#name-input');
+
+const spanEl = document.querySelector('#name-output');
+
+
+inputEl.addEventListener('input', () => {
+    spanEl.textContent = inputEl.value;
+
+    if (inputEl.value === '') {
+        spanEl.textContent = "Anonymous";
+    }
+ });
+
+
+
+console.log(inputEl.value);
