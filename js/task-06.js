60edb4d3ec82ab4b4d0729f1a56a269b1c98bd5c
Maryna Berezhanska <marina@MacBook-Pro-Marina.local>
2022-04-18 00:19:12 +0300

    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,13 @@
 const inputEl = document.querySelector("#validation-input");
 
-const validation = {
-    value: 6,
-};
-
+    
 inputEl.addEventListener('blur', () => {
-    if (inputEl.value.length === validation.value) {
+    if (inputEl.value.length == inputEl.dataset.length) {
         inputEl.classList.add("valid");
     } else {
         inputEl.classList.add("invalid");
+        alert(`Let's try again! ${inputEl.placeholder}`);
+        location.reload();
     }
 });
 
