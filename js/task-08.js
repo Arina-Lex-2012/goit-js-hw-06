3cb4ed90b7ce8f794193aa80e525886d3456ced9
Maryna Berezhanska <marina@MacBook-Pro-Marina.local>
2022-04-17 22:22:28 +0300

    task #9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const formEl = document.querySelector(".login-form");
+const inputEmailEl = document.querySelector("[type=email]");
+const inputPasswordEl = document.querySelector("[type=password]");
+
+const user = {
+    email: "",
+    password: "",
+};
+
+
+formEl.addEventListener('submit', () => {
+    if (inputEmailEl.value === "" && inputPasswordEl.value === "") {
+        console.log('error');
+        alert("Fill in all the fields, please!");
+    } else {
+        // console.log("OK");
+        // alert("OK");
+        user.email = inputEmailEl.value;
+        user.password = inputPasswordEl.value;
+        alert(user.email);
+    }
+
+});
+
+console.log(user);
+
 
